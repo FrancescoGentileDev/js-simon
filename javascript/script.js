f158aee7b1e99bc597b51f5f792843643b6a4e6a
svalvastic <assogames2@gmail.com>
2022-09-05 18:01:32 +0200

    ✨ feat(script): complete script

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 const remember = document.querySelectorAll(".rememberThis");
 const start = document.getElementById("start");
 const countDown = document.querySelector(".counter");
+let timeToWait = 10;
 
 /**
  *
@@ -18,8 +19,8 @@ function randomNumbers(maxNumber, minRange, maxRange) {
   return array;
 }
 
-function startCountDown(time) {
-  let counter = time;
+function startCountDown() {
+  let counter = timeToWait;
   const reference = setInterval(() => {
     counter--;
     countDown.innerHTML = counter;
@@ -32,33 +33,83 @@ function startCountDown(time) {
   return reference;
 }
 
-function hideNumbers(timeToWait) {
+
+function showNumbers(array) {
+    remember.forEach((element, index) => {
+    element.innerHTML = array[index];
+  });
+}
+
+
+function hideNumbers() {
   setTimeout(() => {
     remember.forEach((value) => {
       value.innerHTML = "";
     });
-  }, timeToWait * 1000 + 250);
+  }, timeToWait * 1000);
+}
+
+
+function getSolution(array) {
+    setTimeout(() => {
+        let answerArray = [];
+        array.forEach((value, index) => {
+            answerArray.push(prompt("inserisci numero"))
+        })
+        console.log(answerArray)
+
+        writeResult(answerArray, array)
+  }, timeToWait * 1000);
+}
+/**
+ * 
+ * @param {Array} answers 
+ * @param {Array} results 
+ */
+function writeResult(answers, results) {
+    console.log("risposte:", answers, "soluzioni :", results )
+    let correct = [];
+    let errors = 0;
+
+    for (let i = 0; i < results.length; i++) {
+        let current = results[i].toString()
+        console.log("current", current)
+        
+        if (answers.includes(current)) {
+            console.log("ENTRATO IF")
+            correct.push(current)
+        }
+        else {
+            console.log("ENTRATO ELSE")
+            errors++
+        }
+    }
+    
+    console.log(errors, correct)
+    document.getElementById("error").innerHTML = errors;
+    correct.forEach((value) => {
+        document.getElementById("correct").innerHTML += " " + value
+        showNumbers(results)
+    })
+
+
+
 }
 
 
 
 
 start.addEventListener("click", () => {
-  let timeToWait = 10;
 
   //INIZIO UN COUNTDOWN CHE AGGIORNA IN PAGINA
-    startCountDown(timeToWait);
+    startCountDown();
     
-    //CREO UN ARRAY RANDOM E LI ASSEGNO IN PAGINA
-  let arrayRandom = randomNumbers(5, 1, 99);
-  remember.forEach((element, index) => {
-    element.innerHTML = arrayRandom[index];
-  });
-  
+    //CREO UN ARRAY RANDOM E LO ASSEGNO IN PAGINA
+    let arrayRandom = randomNumbers(5, 1, 99);
+    showNumbers(arrayRandom)
+
   //Nascondo i numeri alla fine del countdown
-    hideNumbers(timeToWait);
+    hideNumbers();
 
-
-
-
+    let answerArray = getSolution(arrayRandom);
 });
